c3abebc26b4b0f5cccdc380d71e8400118f0f99c
Daniel H Crane <daniel.h.crane@outlook.com>
2020-02-12 09:13:56 -0500

    Add project files.

---
+++ Space Trader/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Space_Trader
+{
+    class Class1
+    {
+        public void Run()
+        {
+            Console.WriteLine("This is where your code goes");
+        }
+    }
+}
---
+++ Space Trader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Space_Trader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Class1().Run();
+        }
+    }
+}
